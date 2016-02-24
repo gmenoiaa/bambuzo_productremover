2709059eb16b9bcc40803515a1de22031a280cd3
Geiser Menoia <geiser.menoia@charter.com>
2016-02-23 21:36:02 -0300

    add missing shell file

--- shell/productremover.php
+++ shell/productremover.php
@@ -2,38 +2,85 @@
 require_once 'abstract.php';
 
 /**
- * 
- * @author geiser
  *
+ * @author geiser
+ *        
  */
 class Bambuzo_Productremover_Shell_Productremover extends Mage_Shell_Abstract
 {
-    
+
     private $_delete;
 
     public function run ()
     {
-        $collection = Mage::getSingleton('bambuzo_productremover/notviewedproduct')->getCollection();
-        $count = $collection->getSize();
-        
-        echo "Found $count products to delete\n";
-        
         $this->_delete = $this->getArg('delete') != null;
-    
-        Mage::helper('bambuzo_productremover')->walk($collection, 
-            function  ($product)
-            {
-                return $product->getId();
-            }, 
-            function  ($ids)
-            {
-                echo "Deleting ".count($ids)." products..\n";
-                if($this->_delete) {
-                    Mage::getSingleton('bambuzo_productremover/notviewedproduct')->deleteProducts($ids);
-                }
-            });
         
-        echo "Done.\n";
+        if ($this->getArg('notviewed') != null) {
+            $collection = Mage::getSingleton(
+                    'bambuzo_productremover/notviewedproduct')->getCollection();
+            $count = $collection->getSize();
+            
+            echo "Found $count products to delete\n";
+            
+            Mage::helper('bambuzo_productremover')->walk($collection, 
+                    function  ($product)
+                    {
+                        return $product->getId();
+                    }, 
+                    function  ($ids)
+                    {
+                        echo "Deleting " . count($ids) . " products..\n";
+                        if ($this->_delete) {
+                            Mage::getSingleton(
+                                    'bambuzo_productremover/productremover')->deleteProducts(
+                                    $ids);
+                        }
+                    });
+            
+            echo "Done.\n";
+        } else 
+            if ($this->getArg('file')) {
+                
+                $file = $this->getArg('file');
+                
+                $skus = Mage::getSingleton(
+                        'bambuzo_productremover/productremover')->processCsv(
+                        $file);
+                
+                echo "Found " . count($skus) . " lines\n";
+                
+                if ($this->_delete) {
+                    echo "Deleting products...\n";
+
+                    $model = Mage::getSingleton('bambuzo_productremover/productremover');
+                    $model->setDebug(true);
+                    
+                    list($file,$rows) = $model->deleteProductsBySku(
+                            $skus);
+                    
+                    echo "Created log file $file\n";
+                    echo "Deleted total $rows products\n";
+                }
+                echo "Done.\n";
+            } else {
+                echo $this->usageHelp();
+            }
+    }
+
+    /**
+     * Retrieve Usage Help Message
+     */
+    public function usageHelp ()
+    {
+        return <<<USAGE
+Usage:  php -f productremover.php -- [options]
+    
+  --notviewed                   Deletes all not viewed products in last 90 days
+  --file                        Deletes products from a CSV file with skus
+  --delete                      Set this if you're sure want to remove
+  help                          This help
+    
+USAGE;
     }
 }
 
